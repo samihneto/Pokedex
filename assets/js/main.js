4e42373b7e035598430b22232b341f1213aa8f84
samihneto <samihneto@gmail.com>
2024-01-24 19:57:44 -0300

    pokedex.0.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,30 +14,51 @@ Simplificar o código Fetch: Usar Arrow Function
 Arrow Function: Uma síntaxe reduzida da definição de uma função.
 Não precisa declarar o corpo da função.
 */
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 5
+let offset = 0
 
 
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')} 
-            </ol>
-        
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>
-`
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')} 
+                </ol>
+            
+                <img src="${pokemon.photo}"
+                 alt="${pokemon.name}">
+            </div>
+        </li>
+    `).join('')
+
+
+    pokemonList.innerHTML += newHtml
+    })
 }
 
-    const pokemonList = document.getElementById('pokemonList')
-    
+loadPokemonItens(offset, limit) 
 
-    pokeApi.getPokemons().then((pokemons) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML = newHtml
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
 
-    })
+    const qtdRecordNexPage = offset + limit
+
+    if (qtdRecordNexPage >= maxRecords) {
+        const newLimit = qtdRecordNexPage - maxRecords
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+    loadPokemonItens(offset, limit)
+    }
+})
