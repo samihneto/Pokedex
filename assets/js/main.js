d0a710611eeb53a51b575338c752192c355fb179
samihneto <samihneto@gmail.com>
2024-01-24 02:48:35 -0300

    pokedex.0.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,22 +15,19 @@ Arrow Function: Uma síntaxe reduzida da definição de uma função.
 Não precisa declarar o corpo da função.
 */
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         
         <div class="detail">
             <ol class="types">
-                ${convertPokemonTypesToLi(pokemon.types).join(' ')}
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')} 
             </ol>
         
-            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>
 `
