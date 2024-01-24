ea82d1ef341c384086d6ade95937175d3ae7fc64
samihneto <samihneto@gmail.com>
2024-01-24 02:02:23 -0300

    pokedex0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,24 +15,22 @@ Arrow Function: Uma síntaxe reduzida da definição de uma função.
 Não precisa declarar o corpo da função.
 */
 
-
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
         
         <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join(' ')}
             </ol>
         
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
         </div>
     </li>
 `
@@ -42,16 +40,7 @@ function convertPokemonToLi(pokemon) {
     
 
     pokeApi.getPokemons().then((pokemons) => {
-            const listItems = []
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML = newHtml
 
-            pokemons.map()
-
-            for (let i = 0; i < pokemons.length; i++) {
-                const pokemon = pokemons[i];
-                listItems.push(convertPokemonToLi(pokemon))
-            }
-            
-            console.log(listItems)
-       
-        })
-    .catch((error) => console.error(error))
+    })
